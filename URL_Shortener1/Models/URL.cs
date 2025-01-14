35c16cb592dad1bc204c58240efde7ee6d0b65b1
Max L <tyshykviktoria77@gmail.com>
2025-01-14 17:57:15 +0200

    Add project files.

---
+++ URL_Shortener1/Models/URL.cs
@@ -0,0 +1,13 @@
+﻿namespace URL_Shortener1.Models
+{
+    public class URL
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+
+        public string ShortenedUrl { get; set; }
+        public int CreatedBy { get; set; }
+        public int CreatedDate { get; set; }
+    }
+
+}
---
+++ URL_Shortener1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URL_Shortener1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
